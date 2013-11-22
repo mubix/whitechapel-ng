86a98bd5dcbdcbec08f9b0776553f43b964e922d
Elliott Cutright <elliott.cutright@gmail.com>
2013-11-22 17:50:27 -0500

    Initial rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Whitechapel::Application.config.secret_key_base = '54a23bbbd3cc1fc7d901d9eec60ca7ea960d771b492f49ad1770a89f130234d3cc02d6b6898072d97a858b1907fd684fee886d2bc4a8a648bf0c6b71a37b2ffc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Whitechapel::Application.config.session_store :cookie_store, key: '_whitechapel_session'
