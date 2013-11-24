941662bcb87ac3b7972df5cfeb6c4468ac050a8e
Elliott Cutright <elliott.cutright@gmail.com>
2013-11-24 08:48:50 -0500

    added gitignore file

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(:default, Rails.env)
 
 module Whitechapel
   class Application < Rails::Application
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
